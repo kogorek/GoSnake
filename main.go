0d9a62f8a576b677d0af4181c7fa820a449e0ee5
Igor Lihachev <kogorek@mail.ru>
2018-08-26 12:58:53 +0300

    Created MainLoop and constructor function for MainLoop.
    Created MainLoop and methods: run, update, render, exit.
    Created Factory function for MainLoop

--- main.go
+++ main.go
@@ -1,40 +1,82 @@
 package main
 
 import "github.com/veandco/go-sdl2/sdl"
-import "github.com/EngoEngine/glm"
+// import "github.com/EngoEngine/glm"
 
-func main() {
-	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
-		panic(err)
+import "errors"
+import "os"
+import "fmt"
+
+type MainLoop struct{
+	running bool;
+	window *sdl.Window;
+	renderer *sdl.Renderer;
+}
+
+func (this *MainLoop) exit() {
+	sdl.Quit();
+	if this.window != nil{
+		this.window.Destroy();
 	}
-	defer sdl.Quit()
-
-	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		800, 600, sdl.WINDOW_SHOWN)
-	if err != nil {
-		panic(err)
+	if this.renderer != nil {
+		this.renderer.Destroy();
 	}
-	defer window.Destroy()
+}
 
-	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
-	defer renderer.Destroy()
+func (this *MainLoop) run() {
+	this.running = true;
+	for this.running {
+		this.update();
+		this.draw();
+	}
+}
 
-	// window.UpdateSurface()
-
-	running := true
-	for running {
-		renderer.SetDrawColor(0, 0, 0, 255)
-		renderer.Clear()
-		renderer.SetDrawColor(255, 0, 0, 255)
-	    renderer.DrawLine(0,0,100,100)
-	    renderer.Present()
-		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-			switch event.(type) {
-			case *sdl.QuitEvent:
-				println("Quit")
-				running = false
-				break
-			}
+func (this *MainLoop) update() {
+	for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+		switch event.(type) {
+		case *sdl.QuitEvent:
+			println("Quit")
+			this.running = false
+			break
 		}
 	}
+}
+
+func (this *MainLoop) render() {
+	this.renderer.SetDrawColor(0, 0, 0, 255);
+	this.renderer.Clear();
+	this.renderer.SetDrawColor(255, 0, 0, 255);
+	this.renderer.DrawLine(0,0,100,100);
+	this.renderer.Present();
+}
+
+
+func NewMainLoop() (*MainLoop, error) {
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		return nil, errors.New("Cannot init SDL2");
+	}
+	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+	800, 600, sdl.WINDOW_SHOWN)
+	if err != nil {
+		return nil, errors.New("Cannot create sdl2 window");
+	}
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		return nil, errors.New("Cannot create sdl2 renderer");
+	}
+
+	mainLoop := MainLoop{true, window, renderer};
+
+	return &mainLoop, nil;
+}
+
+func main() {
+	app, err := NewMainLoop();
+	if err != nil {
+		fmt.Println(err);
+		os.Exit(-1);
+	}
+
+	app.run();
+	app.exit();
 }
